87766c8bd8a755a8ea0b893a20f9f15f6c87c8a2
Floris <Floris1996@hotmail.com>
2020-04-03 21:36:56 +0200

    PLAN-45: moves router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import TopNavigation from "./components/TopNavigation";
 import Login from "./containers/Login";
 import Logout from "./containers/Logout";
+import {Provider} from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-      <div>
-        <TopNavigation />
-        <main role="main" className="container">
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/login' component={Login} />
-            <Route path='/logout' component={Logout} />
-          </Switch>
-        </main>
-      </div>
+      <React.StrictMode>
+          <Provider store={store}>
+              <Router>
+                  <div>
+                      <TopNavigation />
+                      <main role="main" className="container">
+                          <Switch>
+                              <Route exact path='/' component={Home} />
+                              <Route path='/login' component={Login} />
+                              <Route path='/logout' component={Logout} />
+                          </Switch>
+                      </main>
+                  </div>
+              </Router>
+          </Provider>
+      </React.StrictMode>
+
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
-import store from './store';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <Router>
-              <App />
-          </Router>
-      </Provider>
-  </React.StrictMode>,
+    <App />
+   ,
   document.getElementById('root')
 );
 
